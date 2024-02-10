6850a63b8f5627129d063717f5804d80f8eb8d4b
BrianDYKim <18k7102dy@gmail.com>
2024-02-10 23:08:31 +0900

    chapter4 추가 완료

---
+++ application/chapter4/src/test/kotlin/team/me/chapter4/Chapter4ApplicationTests.kt
@@ -0,0 +1,11 @@
+package team.me.chapter4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter4ApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,4 +26,4 @@ pluginManagement {
 // modules
 include("common")
 include("core", "core:domain", "core:external")
-include("application", "application:chapter2", "application:chapter3")
+include("application", "application:chapter2", "application:chapter3", "application:chapter4")
