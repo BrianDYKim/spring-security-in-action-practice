7f72a145258b2775b7c980a0cd139889610f22df
BrianDYKim <18k7102dy@gmail.com>
2024-02-24 20:48:54 +0900

    8장 실습 완료

--- application/chapter8/src/main/kotlin/team/me/chapter8/config/SecurityConfiguration.kt
+++ application/chapter8/src/main/kotlin/team/me/chapter8/config/SecurityConfiguration.kt
@@ -2,6 +2,7 @@ package team.me.chapter8.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
@@ -12,20 +13,21 @@ import org.springframework.security.web.SecurityFilterChain
 
 /**
  * @author Doyeop Kim
- * @since 2024/02/18
+ * @since 2024/02/24
  */
 @Configuration
-class SecurityConfig {
+class SecurityConfiguration {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http.httpBasic()
 
         http.authorizeHttpRequests {
-            // 홀수 유저만 en 접근 가능
-            it.requestMatchers("/api/v1/hello/en").hasRole("ADMIN")
-            // 짝수 유저만 cn 접근 가능
-            it.requestMatchers("/api/v1/hello/cn").hasRole("MANAGER")
+            it.requestMatchers(HttpMethod.GET, "/a").authenticated()
+                .requestMatchers(HttpMethod.POST, "/a").permitAll()
+                .anyRequest().permitAll()
         }
+
+        http.csrf { it.disable() }
 
         return http.build()
     }
@@ -34,15 +36,20 @@ class SecurityConfig {
     fun userDetailsService(): UserDetailsService {
         val manager = InMemoryUserDetailsManager()
 
-        // 유저 등록 (짝수: READ, 홀수: WRITE)
-        List(5) { it }
-            .map {
-                return@map when (it % 2) {
-                    0 -> User.withUsername("user$it").password("$it").roles("MANAGER").build()
-                    else -> User.withUsername("user$it").password("$it").roles("ADMIN").build()
-                }
-            }
-            .forEach { manager.createUser(it) }
+        val user1 =
+            User.withUsername("john")
+                .password("12345")
+                .roles("ADMIN")
+                .build()
+
+        val user2 =
+            User.withUsername("jane")
+                .password("12345")
+                .roles("MANAGER")
+                .build()
+
+        manager.createUser(user1)
+        manager.createUser(user2)
 
         return manager
     }
