87566c1a5bbbe218a3f2e240c2d0d3570e3139f1
BrianDYKim <18k7102dy@gmail.com>
2024-03-03 14:43:35 +0900

    appliaction-prod.yml을 제외

--- application/chapter13-authorization/build.gradle.kts
+++ application/chapter13-authorization/build.gradle.kts
@@ -1,2 +1,17 @@
+apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+
 dependencies {
+    implementation(project(":common"))
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    testImplementation("org.springframework.security:spring-security-test")
+
+    // thymeleaf
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+
+    // oAuth
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 }
